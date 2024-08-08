89f0b68a925aa61e3d6de36befc8b7cbef8ef5b4
AL-HussienAbdAlrazeq <hessenabdalrazek1@gmail.com>
2024-08-08 03:18:06 +0300

    Sara7a_App MVC

--- database/DB.Connection.js
+++ database/DB.Connection.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 
 
-export const dbConnection = connect(`mongodb://localhost:27017/Sara7a_MVC`).
+export const dbConnection = connect(`mongodb+srv://Sara7a_App_MVC:iW3Lb6EbQ175Hp5y@cluster0.pvn4zwe.mongodb.net/Sara7a_MVC`).
 then(()=>{
     console.log("DataBase Connected")
 })
